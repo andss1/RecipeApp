7a4ca6d12a1a9d880706dbfed7d25c5dec063470
Anderson Simao <simao.anderson4@gmail.com>
2022-06-02 10:58:04 -0300

    Create splash screen

--- app/src/main/java/com/example/recipeapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/recipeapp/SplashScreenActivity.kt
@@ -3,9 +3,9 @@ package com.example.recipeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splashscreen)
     }
 }
